967b9eb2028531d52cdf6f3a04170d8ee1b568fb
sskerdi <skerdicuka7@gmail.com>
2026-05-15 19:00:09 +0000

    idk

--- src/main/scala/circuits/helpers/FullAdder.scala
+++ src/main/scala/circuits/helpers/FullAdder.scala
@@ -11,5 +11,18 @@ class FullAdder extends Module {
   val sum  = IO(Output(Bool()))
   val cout = IO(Output(Bool()))
 
-  ???
+  val ha1    = Module(new HalfAdder)
+  val ha2    = Module(new HalfAdder)
+  val OrGate = Module(new ORGate)
+
+  ha1.a    := a
+  ha1.b    := b
+  ha2.a    := ha1.sum
+  ha2.b    := cin
+  sum      := ha2.sum
+  OrGate.a := ha2.cout
+  OrGate.b := ha1.cout
+
+  cout := OrGate.out
+
 }
